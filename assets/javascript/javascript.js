f06a77e29dada8dd204820abb90bcf9371a4caef
zazil <zaziltoma@gmail.com>
2017-08-23 00:13:51 -0700

    added html, css and js

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,43 @@
+document.ready {
+
+var game = {
+		wins: 0,
+		losses: 0,
+		numberTarget: null,
+		totalScore: 0,
+
+		startGame: function () {
+			// generate random number
+			// reset random coin values
+		},
+
+		addCoin: function (coinValue) {
+				// add value of coin to total score 
+		},
+
+		winLose: function () {
+			// if totalScore = numberTarget then alert you win
+				// wins++
+			// else if totalScore >numberTarget then alert you lose
+				//lose++
+		}
+};
+
+$("#newGame").on("click", function {
+	game.startGame();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+}
