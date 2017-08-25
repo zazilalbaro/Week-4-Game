4adf37618bd23b8a75e01872f5d9dc3a210e5a55
zazil <zaziltoma@gmail.com>
2017-08-24 21:39:35 -0700

    coin value broken

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,31 @@
-document.ready {
+// document.ready() {
+window.onload = function() {
 
 var game = {
 		wins: 0,
 		losses: 0,
 		numberTarget: null,
 		totalScore: 0,
+		coins: {
+			coin1: null, 
+			coin2: null, 
+			coin3: null, 
+			coin4: null
+		},
 
 		startGame: function () {
 			// generate random number
+			numberTarget = this.generateNumber(19, 120)
+			$("#targetNum").html(numberTarget)
 			// reset random coin values
+			for (i=0; i<game.coins.length; i++) {
+				this.coins[i] = this.generateNumber(1, 12)
+			}
+			$("#coin1").attr("value", this.coins[0])
+			$("#coin2").attr("value", this.coins[1])
+			$("#coin3").attr("value", this.coins[2])
+			$("#coin4").attr("value", this.coins[3])
+			console.log(this.coins)
 		},
 
 		addCoin: function (coinValue) {
@@ -20,14 +37,19 @@ var game = {
 				// wins++
 			// else if totalScore >numberTarget then alert you lose
 				//lose++
+		},
+
+		generateNumber: function(min, max) {
+     		return Math.floor(Math.random()*(max-min))+min;
 		}
-};
 
-$("#newGame").on("click", function {
+	
+}; //end of object	
+
+
+
+$("#newGame").on("click", function() {
 	game.startGame();
-
-
-
 });
 
 
@@ -40,4 +62,9 @@ $("#newGame").on("click", function {
 
 
 
-}
+
+
+
+
+
+};
