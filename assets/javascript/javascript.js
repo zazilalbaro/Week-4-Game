e70b0f94fe6a2d4e74d1fbb8782a388a5d3adb79
zazil <zaziltoma@gmail.com>
2017-08-24 22:46:33 -0700

    working target, coin values and total score

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,31 +5,34 @@ var game = {
 		wins: 0,
 		losses: 0,
 		numberTarget: null,
-		totalScore: 0,
-		coins: {
-			coin1: null, 
-			coin2: null, 
-			coin3: null, 
-			coin4: null
-		},
+		total: 0,
+		coins: [],
+		numOfCoins: 4,
+		gameOn: false,
 
 		startGame: function () {
 			// generate random number
 			numberTarget = this.generateNumber(19, 120)
 			$("#targetNum").html(numberTarget)
 			// reset random coin values
-			for (i=0; i<game.coins.length; i++) {
-				this.coins[i] = this.generateNumber(1, 12)
+			var random;
+			for (i=0; i<this.numOfCoins; i++) {
+				random = this.generateNumber(1, 12);
+				this.coins.push(random);
 			}
 			$("#coin1").attr("value", this.coins[0])
 			$("#coin2").attr("value", this.coins[1])
 			$("#coin3").attr("value", this.coins[2])
 			$("#coin4").attr("value", this.coins[3])
-			console.log(this.coins)
+
+			this.gameOn = true
 		},
 
 		addCoin: function (coinValue) {
 				// add value of coin to total score 
+					this.total += coinValue;
+					$("#totalScore").html(this.total);
+					// console.log(this.total);
 		},
 
 		winLose: function () {
@@ -50,6 +53,17 @@ var game = {
 
 $("#newGame").on("click", function() {
 	game.startGame();
+});
+
+$(".img").on("click", function() {
+	if (game.gameOn) {
+	var coinValue = parseInt($(this).attr("value"));
+	// console.log(coinValue);
+	game.addCoin(coinValue);
+	}
+	else {
+		alert("Please click on Start Game");
+	}
 });
 
 
