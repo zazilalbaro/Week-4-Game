c2646b05c29aed5a98946285c4167013463341bf
zazil <zaziltoma@gmail.com>
2017-08-25 22:17:12 -0700

    game complete!

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,9 +11,12 @@ var game = {
 		gameOn: false,
 
 		startGame: function () {
+			// reset values just in case the previous game didn't end
+			this.resetGame();
 			// generate random number
-			numberTarget = this.generateNumber(19, 120)
-			$("#targetNum").html(numberTarget)
+			this.numberTarget = this.generateNumber(19, 120)
+			$("#targetNum").html(this.numberTarget)
+			$("#totalScore").html("-");
 			// reset random coin values
 			var random;
 			for (i=0; i<this.numOfCoins; i++) {
@@ -25,28 +28,58 @@ var game = {
 			$("#coin3").attr("value", this.coins[2])
 			$("#coin4").attr("value", this.coins[3])
 
+			console.log(this.coins);
 			this.gameOn = true
 		},
 
 		addCoin: function (coinValue) {
-				// add value of coin to total score 
-					this.total += coinValue;
-					$("#totalScore").html(this.total);
-					// console.log(this.total);
+			// add value of coin to total score 
+			this.total += coinValue;
+			$("#totalScore").html(this.total);
+			console.log(this.total);
+			console.log(this.numberTarget);
+			// win/lose?
+			if (this.total === this.numberTarget) {
+				game.winLose("win");
+			}
+			else if (this.total > this.numberTarget) {
+				game.winLose("lose");
+			}
 		},
 
-		winLose: function () {
-			// if totalScore = numberTarget then alert you win
+		winLose: function (endStatus) {
+			// if total= numberTarget then alert you win
 				// wins++
-			// else if totalScore >numberTarget then alert you lose
+			if (endStatus === "win") {
+				alert ("You win! Great job!");
+				this.wins++;
+				console.log(this.wins);
+				$("#wins").html("Wins: " + this.wins);
+				this.resetGame();
+			}
+			// else if total >numberTarget then alert you lose
 				//lose++
+			else if (endStatus === "lose") {
+				alert("Oops, you went over the target. Try again!");
+				this.losses++;
+				console.log(this.losses);
+				$("#losses").html("Losses: " + this.losses);
+				this.resetGame();
+			}	
 		},
 
 		generateNumber: function(min, max) {
      		return Math.floor(Math.random()*(max-min))+min;
-		}
+		},
 
-	
+		resetGame: function() {
+			this.gameOn = false;
+			this.numberTarget = null;
+			this.coins = [];
+			this.total = 0;
+			// $("#targetNum").html("-");
+			// $("#totalScore").html("-");
+		}
 }; //end of object	
 
 
@@ -62,7 +95,7 @@ $(".img").on("click", function() {
 	game.addCoin(coinValue);
 	}
 	else {
-		alert("Please click on Start Game");
+		alert("Please click on the Start Game button");
 	}
 });
 
